363e6efb2c237144dd4b521e73600dc73ba5a271
yash.bhandare1 <yash.bhandare@accelya.com>
2026-09-13 14:12:53 +0530

    Add Code Practice files

---
+++ Code Practice/Basics/Sum_of_numbers.java
@@ -0,0 +1,10 @@
+class Sum_of_numbers{
+    public static void main (String[] args){
+        int n = 5;
+        int sum = 0;
+        for (int i=1; i<=n;i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    }
+}
