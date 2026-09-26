c9a52e0508c00337cc11c9b1be727e743607bf70
Yash Bhandare <yashbhandare0203@gmail.com>
2026-09-26 15:17:03 +0530

    BubbleSort

--- Code Practice/Basic Sorting Algorithm/BubbleSort.java
+++ Code Practice/Basic Sorting Algorithm/BubbleSort.java
@@ -1,4 +1,15 @@
 public class BubbleSort{
+    
+    /*
+    Initial: {5, 4, 1, 3, 2}
+
+    ↓ Bubble Sort
+
+    Pass 1 → {4, 1, 3, 2, 5}   5 moved to end
+    Pass 2 → {1, 3, 2, 4, 5}
+    Pass 3 → {1, 2, 3, 4, 5}
+    Pass 4 → {1, 2, 3, 4, 5}
+    */
 
     public static void bubbleSort(int arr[]){
         for(int turn=0; turn<arr.length-1; turn++){
