1a17211b200bd413b21c4e055997c286f2f3d1ce
Yash Bhandare <yashbhandare0203@gmail.com>
2026-09-26 11:04:14 +0530

    Move array programs to Array directory

--- Code Practice/Basics/Star_pattern.java
+++ Code Practice/Basics/Star_pattern.java
@@ -3,8 +3,8 @@ class Star_pattern{
         int n = 3;
         //char ch = 'A';
         for (int i =1; i<=n;i++){
-            //for (int j=1; j<=i;j++){ //Stars
-            for (int j=1; j<=n-i+1;j++){
+            for (int j=1; j<=i;j++){ //Stars
+            //for (int j=1; j<=n-i+1;j++){ //Inverted Stars
                 System.out.print("*");
                 //System.out.print(j); numbers
                 //System.out.print(ch); charcters
